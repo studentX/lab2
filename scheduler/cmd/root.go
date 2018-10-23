8b82443f053ea24c74b96a928c8246223b6621c5
k8sland <fernand@k8sland.io>
2018-10-23 08:37:05 -0600

    updates

---
+++ scheduler/cmd/root.go
@@ -0,0 +1,225 @@
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"syscall"
+
+	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	app       = "PartyScheduler"
+	namespace = "default"
+	schedName = "partysched"
+)
+
+var (
+	// Version set via build tags
+	Version = ""
+	srv     kubernetes.Interface
+
+	attires = []string{"ghoul", "goblin"}
+	rootCmd = &cobra.Command{
+		Use:   strings.ToLower(app),
+		Short: "Schedules pods based on costumes",
+		Long:  "Schedules pods based on costumes",
+		Run:   listen,
+	}
+)
+
+// Execute runs the command
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Version = Version
+}
+
+func setupSignal(cancel context.CancelFunc) {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case <-signalChan:
+			log.Printf("Shutdown signal received, exiting...")
+			cancel()
+			os.Exit(0)
+		}
+	}
+}
+
+func listen(cmd *cobra.Command, args []string) {
+	opts := metav1.ListOptions{
+		FieldSelector: "spec.nodeName=",
+	}
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
+	go setupSignal(cancel)
+
+	c := make(chan *corev1.Pod)
+	go scheduler(ctx, c)
+
+	w, err := apiServer().CoreV1().Pods(namespace).Watch(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%s is listening for pods...", app)
+	for e := range w.ResultChan() {
+		p := e.Object.(*corev1.Pod)
+		if p.Spec.SchedulerName != schedName {
+			continue
+		}
+		log.Println("Unscheduled pod detected", p.ObjectMeta.Name)
+		switch e.Type {
+		case watch.Added:
+			c <- p
+		}
+	}
+}
+
+func scheduler(ctx context.Context, c <-chan *corev1.Pod) {
+	for {
+		select {
+		case p := <-c:
+			var (
+				nodes corev1.NodeList
+				err   error
+			)
+			if nodes, err = checkFit(p); err != nil {
+				log.Println(err)
+				break
+			}
+			if len(nodes.Items) == 0 {
+				log.Printf("Unable to fit pod %s on any nodes", p.ObjectMeta.Name)
+				break
+			}
+			for _, n := range nodes.Items {
+				if err := schedule(n, p); err != nil {
+					log.Fatal(err)
+				}
+				break
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func schedule(n corev1.Node, p *corev1.Pod) error {
+	body := map[string]interface{}{
+		"target": map[string]string{
+			"kind":       "Node",
+			"apiVersion": "v1",
+			"name":       n.ObjectMeta.Name,
+			"namespace":  namespace,
+		},
+		"metadata": map[string]string{
+			"name":      p.ObjectMeta.Name,
+			"namespace": namespace,
+		},
+	}
+	data, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	bind := apiServer().
+		CoreV1().
+		RESTClient().
+		Post().
+		Namespace(namespace).
+		Resource("bindings").
+		Body(data)
+	var b corev1.Binding
+	if err := bind.Do().Into(&b); err != nil {
+		return err
+	}
+	log.Printf("Ye! scheduled pod %s onto node %s", p.ObjectMeta.Name, n.ObjectMeta.Name)
+	return nil
+}
+
+func checkFit(p *corev1.Pod) (corev1.NodeList, error) {
+	candidates := corev1.NodeList{}
+
+  var (
+		costume string
+		ok bool
+	)
+	if costume, ok = p.ObjectMeta.Labels["costume"]; !ok {
+		log.Printf("Party pooper detected! No costume on pod %s", p.ObjectMeta.Name)
+		return candidates, nil
+	}
+
+	nn, err := getNodes()
+	if err != nil {
+		return candidates, err
+	}
+
+	for _, n := range nn.Items {
+		if trickOrTreat(costume) {
+			log.Printf("Found match!! %s", p.ObjectMeta.Name)
+			candidates.Items = append(candidates.Items, n)
+		} else {
+			log.Printf("Improper attire on pod %s", p.ObjectMeta.Name)
+		}
+	}
+	return candidates, nil
+}
+
+func trickOrTreat(costume string) bool {
+	for _, c := range attires {
+		if c == costume {
+			return true
+		}
+	}
+	return false
+}
+
+func getNodes() (*corev1.NodeList, error) {
+	return apiServer().CoreV1().Nodes().List(metav1.ListOptions{})
+}
+
+func apiServer() kubernetes.Interface {
+	if srv != nil {
+		return srv
+	}
+
+	var (
+		cfg *restclient.Config
+		err error
+	)
+	path := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	if _, err = os.Stat(path); os.IsNotExist(err) {
+		cfg, err = restclient.InClusterConfig()
+	} else {
+		cfg, err = clientcmd.BuildConfigFromFlags("", path)
+	}
+	if err != nil {
+		log.Panic(err)
+	}
+
+	srv, err = kubernetes.NewForConfig(cfg)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return srv
+}
---
+++ scheduler/go.mod
@@ -0,0 +1,34 @@
+module github.com/k8sland/lab2/scheduler
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/gogo/protobuf v1.1.1 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
+	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
+	github.com/googleapis/gnostic v0.2.0 // indirect
+	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.5 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20181015023909-0c41d7ab0a0e // indirect
+	golang.org/x/net v0.0.0-20181017193950-04a2e542c03f // indirect
+	golang.org/x/oauth2 v0.0.0-20181017192945-9dcd33a902f4 // indirect
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
+	golang.org/x/sys v0.0.0-20181019160139-8e24a49d80f8 // indirect
+	golang.org/x/time v0.0.0-20180412165947-fbb02b2291d2 // indirect
+	google.golang.org/appengine v1.2.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.2.1 // indirect
+	k8s.io/api v0.0.0-20181018013834-843ad2d9b9ae
+	k8s.io/apimachinery v0.0.0-20181015213631-60666be32c5d
+	k8s.io/client-go v9.0.0+incompatible
+)
