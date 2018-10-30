cf9521d5fb67bad23b3f2362c1ea4e93df4ca461
k8sland <fernand@k8sland.io>
2018-10-29 21:01:22 -0700

    updates

--- istio/picker.sh
+++ istio/picker.sh
@@ -3,5 +3,7 @@ while true; do
   clear
   echo "Hangman"
   http --print=b --pretty=colors $(minikube ip):31380/new_game
+  # OR if no httpie...
+  # curl $(minikube ip):31380/new_game
   sleep ${1:-2}
 done
