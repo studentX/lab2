17295afcf23b1737d53414710008263cc2a19208
k8sland <fernand@k8sland.io>
2018-12-02 22:33:29 -0800

    updates

--- hpa/burst.sh
+++ hpa/burst.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-while true; do
-  clear
-  http --print=b --pretty=colors $(minikube ip):30400/graphql query='{movies{name}}'
-  sleep ${1:-1}
+for i in {1..100}; do \
+  curl -s -H "Content-Type: application/json" -d '{"query":"{movies{name}}"}' http://$(minikube ip):30400/graphql; \
 done
-
-curl -H "Content-Type: application/json" -d '{"query":"{movies{name}}"}' http://$(minikube ip):30400/graphql
-hey -c 1 -n 1 -m POST -H "Content-Type: application/json" -d '{"query":"{movies{name}}"}' http://$(minikube ip):30400/graphql
