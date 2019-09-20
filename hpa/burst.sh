a8c23f6f52c0416fd4aa6f1070be56cdc6d007de
k8sland <fernand@k8sland.io>
2019-09-19 21:42:40 -0600

    updates

--- hpa/burst.sh
+++ hpa/burst.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for i in {1..100}; do \
-  curl -s -H "Content-Type: application/json" -d '{"query":"{movies{name}}"}' http://$(minikube ip):30400/graphql; \
+for i in {1..300}; do \
+  curl -H "Content-Type: application/json" -d '{"query":"{movies{name}}"}' http://$(minikube ip):30401/graphql; \
 done
